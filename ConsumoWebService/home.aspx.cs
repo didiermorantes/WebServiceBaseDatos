dd328f0cf24ed32dde4af6bcec5c37049f5f0d90
didiermorantes <didiermorantes@gmail.com>
2019-07-26 12:07:13 -0500

    web service base datos

---
+++ ConsumoWebService/home.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumoWebService
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebServiceBaseDatos.WebService1SoapClient WS = new WebServiceBaseDatos.WebService1SoapClient();
+            DataSet ds = WS.GetData();
+           
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ WebServiceBaseDatos/WebService1.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+
+namespace WebServiceBaseDatos
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public DataSet GetData()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source=DESKTOP-R3GF3UI; Initial Catalog=Sivigila; Integrated Security=True;";
+            SqlDataAdapter da = new SqlDataAdapter("SELECT TOP (5) [nombreTabla],[nombre],[jsEvento] FROM TablaTipoEvento",conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
